a8999fbd0c47b1fd7e8fa24398d2e4ba0e76266e
Jon Errasti <jon44errasti@gmail.com>
2025-01-29 19:06:46 +0100

    Sligh modifivations.

--- scripts/run_simulation.py
+++ scripts/run_simulation.py
@@ -5,9 +5,10 @@ def main():
     simulation_params = {
         'initial_portfolio': 1.5e06,
         'asset_allocation': {'stocks': 1, 'bonds': 0},
-        'annual_withdrawal': -60000,
-        'retirement_years': 10,
-        'n_simulations': 2000
+        'annual_withdrawal': 60000,
+        'retirement_years': 62,
+        'n_simulations': 2000,
+        'tickers': ['^GSPC', '^FVX', '^TNX']
     }
 
     results, depletion_risk, best_case, worst_case = run_retirement_simulation(simulation_params)
